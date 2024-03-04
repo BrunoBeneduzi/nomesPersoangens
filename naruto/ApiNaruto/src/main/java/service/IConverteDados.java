9feef7aafad0c6e6b7af14f64c3dff1f0eb3986c
Bruno Beneduzi <124108326+BrunoBeneduzi@users.noreply.github.com>
2024-03-04 15:51:03 -0300

    Update IConverteDados.java

--- naruto/ApiNaruto/src/main/java/service/IConverteDados.java
+++ naruto/ApiNaruto/src/main/java/service/IConverteDados.java
@@ -3,7 +3,7 @@ package service;
 import java.util.List;
 
 public interface IConverteDados {
-	<T> T obterDados(String json, Class<T> classe);//recebe um Json e converte em uma classe manipulavem
+	<T> T obterDados(String json, Class<T> classe);//recebe um Json e converte em uma classe manipulavel
 	
 	
 	<T> List<T> obterLista(String json, Class<T> classe);//cria uma lista
